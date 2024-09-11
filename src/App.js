03b953090f0110b0155af53d258092fcad4ee738
Ravi Kant <ravitafe40@gmail.com>
2024-09-12 00:00:14 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [phoneNumber , setPhoneNumber] = useState('');
+
+  const [headerData , setHeaderData] = useState(null);
+
+  const handleChange = (e) =>{
+    setPhoneNumber(e.target.value)
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+
+    try{
+      const response = await axios.post('https://chimpu.online/api/post.php',{phonenumber:phoneNumber});
+
+      const headers = response.data;
+
+      setHeaderData(headers);
+
+    }
+    catch (err){
+
+      console.log('Error posting data',err)
+
+    }
+
+  }
+  console.log(phoneNumber)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 className='heading'>Post Phone Number</h1>
+      
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={phoneNumber} onChange={handleChange} placeholder='Enter the phone number'/>
+        <button type='submit'>Submit</button>
+      </form>
+
+      {headerData && (
+        <div>
+          <h2>Response Headers :</h2>
+          <p>{headerData.msg}</p>
+          <p>{headerData.error}</p>
+          <p>{headerData.error_code}</p>
+        </div>
+      )}
     </div>
   );
 }
