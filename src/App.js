7dbaad6eb550f90a58186ffd853fca81504d112d
Ravi Kant <ravitafe40@gmail.com>
2024-09-14 01:42:16 +0530

    Validation of input

--- src/App.js
+++ src/App.js
@@ -9,11 +9,21 @@ function App() {
   const [headerData , setHeaderData] = useState(null);
 
   const handleChange = (e) =>{
-    setPhoneNumber(e.target.value)
+    // const value = e.target.value;
+    // if (/^\d*$/.test(value)) {
+    //   setPhoneNumber(value);
+    // }
+    setPhoneNumber(e.target.value);
   }
 
   const handleSubmit = async (e) =>{
     e.preventDefault();
+  
+    if (phoneNumber.length <10) {
+      alert('Please enter 10 digit phone number');
+      setPhoneNumber('');
+      return;
+    }
 
     try{
       const response = await axios.post('https://chimpu.online/api/post.php',{phonenumber:phoneNumber});
@@ -29,6 +39,10 @@ function App() {
 
     }
 
+    finally{
+      setPhoneNumber('');
+    }
+
   }
   console.log(phoneNumber)
 
@@ -38,8 +52,8 @@ function App() {
       <h1 className='heading'>Post Phone Number</h1>
       
       <form onSubmit={handleSubmit}>
-        <input type='text' value={phoneNumber} onChange={handleChange} placeholder='Enter the phone number'/>
-        <button type='submit'>Submit</button>
+        <input type='text' value={phoneNumber} onChange={handleChange} placeholder='Enter the phone number' className='input' pattern='\d*'/>
+        <button type='submit' className='btn'>Submit</button>
       </form>
 
       {headerData && (
